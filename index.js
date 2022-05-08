3236d52564f45b38d246494a6d559a9861e10945
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-05-08 20:51:14 -0300

    refact calc value of transactions and balance

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.post("/sing-in", async (req, res) => {
       user: user._id,
     });
 
-    return res.status(200).send(token);
+    return res.status(200).send({ token });
   } else {
     // o usuario nao existe ou a senha esta incorreta
     return res.sendStatus(401);
@@ -82,9 +82,46 @@ app.get("/transactions", async (req, res) => {
       .find({ user: user._id })
       .toArray();
 
-    const value = transactions.reduce((acc, curr) => acc + curr.value, 0);
+    // let value = transactions.reduce((acc, curr) => {
+    //   return acc + curr.value;
+    // }, 0);
 
-    const balance = { inOut: value >= 0 ? "positive" : "negative", value };
+    // //transformando o valor para moeda brl
+    // value = value.toLocaleString("pt-BR", {
+    //   style: "currency",
+    //   currency: "BRL",
+    // });
+    let value = 0;
+    let income = 0;
+    let outcome = 0;
+
+    transactions.forEach((transaction) => {
+      let realValue = transaction.value
+        .replace("R$", "")
+        .replace(",", ".")
+        .trim();
+
+      if (realValue.includes("-")) {
+        realValue = realValue.replace("-", "").trim();
+        outcome += Number(realValue);
+      } else {
+        income += Number(realValue);
+      }
+
+      realValue = Number(realValue);
+    });
+
+    value = income - outcome;
+
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    const balance = {
+      inOut: income >= outcome ? "positive" : "negative",
+      value,
+    };
 
     return res.status(200).send({ transactions, balance, name: user.name });
   } catch (error) {
@@ -95,7 +132,7 @@ app.get("/transactions", async (req, res) => {
 
 app.post("/transactions", async (req, res) => {
   const { authorization } = req.headers;
-  const { value, description } = req.body;
+  let { value, description, type } = req.body;
 
   let token = authorization?.replace("Bearer ", "");
 
@@ -115,12 +152,19 @@ app.post("/transactions", async (req, res) => {
       month: "2-digit",
     });
 
+    //transformando o valor para moeda brl
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+
     const transaction = {
       _id: new ObjectId(),
       user: user._id,
       value,
       description,
       date,
+      type,
     };
 
     await db.collection("transactions").insertOne(transaction);
